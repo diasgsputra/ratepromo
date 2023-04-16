bb9d8fae7736343f58f5cb97101dbaddad51effe
diasgsputra <diasgsputra@gmail.com>
2023-04-17 00:11:05 +0700

    cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const koneksi = require('./config/database');
 const app = express();
+const cors = require("cors")
 const PORT = process.env.PORT || 5000;
 // set body parser
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
