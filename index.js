b6c31b68af8a80530160270ad9c8383fb5f937ff
diasgsputra <diasgsputra@gmail.com>
2023-05-02 15:14:17 +0700

    sort by provider

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/promo', (req, res) => {
 
 // read data / get data
 app.get('/promo', (req, res) => {
-    const querySql = 'SELECT * FROM promo_provider WHERE isActive = 1 SORT BY provider DESC';
+    const querySql = 'SELECT * FROM promo_provider WHERE isActive = 1 ORDER BY provider DESC';
 
     koneksi.query(querySql, (err, rows, field) => {
         if (err) {
